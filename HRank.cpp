9a2e7beba5f136f0c234de5172113a33dc6610e1
Manjunath Shivanna <ms5655@rit.edu>
2017-08-12 20:35:53 -0400

    started working on hackerrank

---
+++ HRank.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "HRank.h"
+
+
+HRank::HRank()
+{
+}
+
+
+HRank::~HRank()
+{
+}
---
+++ HRank.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+class HRank
+{
+public:
+	HRank();
+	~HRank();
+
+	int solveMeFirst(int a, int b) {
+		// Hint: Type return a+b; below:
+
+		return 0;
+
+	}
+
+	int main() {
+		int num1, num2;
+		int sum;
+		//cin >> num1 >> num2;
+		sum = solveMeFirst(num1, num2);
+		//cout << sum;
+		return 0;
+	}
+
+};
+
